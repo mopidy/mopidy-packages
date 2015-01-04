f7bc159522981853f835e3ddf8cb6c8e7a548c39
Stein Magnus Jodal <stein.magnus@jodal.no>
2015-01-04 20:58:55 +0100

    Add APIs to expose people and projects

--- mopidy_packages/models.py
+++ mopidy_packages/models.py
@@ -1,3 +1,81 @@
+import json
+import logging
+import pathlib
+
+import jsonschema
 
 
-# TODO: Add database models
+logger = logging.getLogger(__name__)
+
+
+ROOT_DIR = pathlib.Path(__file__).parent.parent
+
+
+class ModelException(Exception):
+    pass
+
+
+class Model:
+    _schema_cache = None
+
+    @classmethod
+    def all(cls):
+        for path in cls.DIR.glob(cls.DATA_GLOB):
+            if path == cls.SCHEMA_FILE:
+                continue
+            data = cls._get_data(path)
+            if data is None:
+                continue
+            yield cls(data)
+
+    @classmethod
+    def get(cls, name):
+        path = cls.DIR / (cls.DATA_FORMAT % name)
+        data = cls._get_data(path)
+        if data is None:
+            return None
+        return cls(data)
+
+    @classmethod
+    def _get_data(cls, path):
+        if not path.exists():
+            return None
+
+        with path.open() as fh:
+            data = json.load(fh)
+
+        try:
+            jsonschema.validate(
+                data, cls._get_schema(),
+                format_checker=jsonschema.FormatChecker())
+        except jsonschema.ValidationError as exc:
+            raise ModelException('Invalid JSON structure: %s' % exc) from exc
+        else:
+            return data
+
+    @classmethod
+    def _get_schema(cls):
+        if cls._schema_cache is None:
+            with cls.SCHEMA_FILE.open() as fh:
+                cls._schema_cache = json.load(fh)
+        return cls._schema_cache
+
+    def __init__(self, data):
+        self.data = data
+
+    def to_json(self):
+        return self.data
+
+
+class Person(Model):
+    DIR = ROOT_DIR / 'people'
+    DATA_GLOB = '*.json'
+    DATA_FORMAT = '%s.json'
+    SCHEMA_FILE = DIR / 'person.schema.json'
+
+
+class Project(Model):
+    DIR = ROOT_DIR / 'projects'
+    DATA_GLOB = '*/project.json'
+    DATA_FORMAT = '%s/project.json'
+    SCHEMA_FILE = DIR / 'projects.schema.json'
--- mopidy_packages/views.py
+++ mopidy_packages/views.py
@@ -1,6 +1,88 @@
-from . import app
+import flask
+
+from . import app, models
+
+
+api_endpoints = []
+
+
+def api_endpoint(func):
+    """Decorator for adding a view to the list of public API endpoints"""
+    api_endpoints.append(func)
+    return func
 
 
 @app.route('/')
-def hello():
-    return 'Hello, world!'
+def index():
+    return flask.redirect(flask.url_for('list_api_endpoints'))
+
+
+@app.route('/api')
+def list_api_endpoints():
+    url_rules = {f.endpoint: f.rule for f in app.url_map.iter_rules()}
+
+    return flask.jsonify(endpoints={
+        func.__name__: {
+            'url': url_rules[func.__name__],
+            'description': func.__doc__,
+        }
+        for func in api_endpoints
+    })
+
+
+@api_endpoint
+@app.route('/api/people')
+def list_people():
+    """Returns a list of people in the Mopidy community"""
+
+    try:
+        people = [p.to_json() for p in models.Person.all()]
+    except models.ModelException as exc:
+        return flask.Response(str(exc), status=500, content_type='text/plain')
+
+    return flask.jsonify(people=people)
+
+
+@api_endpoint
+@app.route('/api/people/<name>')
+def get_person(name):
+    """Returns detailed information about a specific person"""
+
+    try:
+        person = models.Person.get(name)
+    except models.ModelException as exc:
+        return flask.Response(str(exc), status=500, content_type='text/plain')
+
+    if person is None:
+        flask.abort(404)
+
+    return flask.jsonify(person.to_json())
+
+
+@api_endpoint
+@app.route('/api/projects')
+def list_projects():
+    """Returns a list of projects in the Mopidy ecosystem"""
+
+    try:
+        projects = [p.to_json() for p in models.Project.all()]
+    except models.ModelException as exc:
+        return flask.Response(str(exc), status=500, content_type='text/plain')
+
+    return flask.jsonify(projects=projects)
+
+
+@api_endpoint
+@app.route('/api/projects/<name>')
+def get_project(name):
+    """Returns detailed information about a specific project"""
+
+    try:
+        project = models.Project.get(name)
+    except models.ModelException as exc:
+        return flask.Response(str(exc), status=500, content_type='text/plain')
+
+    if project is None:
+        flask.abort(404)
+
+    return flask.jsonify(project.to_json())
