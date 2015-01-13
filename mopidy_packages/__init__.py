d987be307f7985f66a4baa784598f2a92f868ef4
Stein Magnus Jodal <stein.magnus@jodal.no>
2015-01-13 15:57:23 +0100

    Remove database stuff

--- mopidy_packages/__init__.py
+++ mopidy_packages/__init__.py
@@ -1,14 +1,10 @@
 import os
 
 import flask
-from flask.ext.sqlalchemy import SQLAlchemy
 
 
 app = flask.Flask(__name__)
 app.config['DEBUG'] = 'DEBUG' in os.environ
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-
-db = SQLAlchemy(app)
 
 
 from . import models  # noqa: Import models to register them
