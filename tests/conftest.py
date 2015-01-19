460ffdfca52782393a7c9bd05dda60bd1f59d3bd
Stein Magnus Jodal <stein.magnus@jodal.no>
2015-01-19 01:43:12 +0100

    Test static web views

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,12 +2,18 @@ from unittest import mock
 
 import pytest
 
-from mopidy_packages import models, web
+from mopidy_packages import models, web, web_static
 
 
 @pytest.fixture
 def app():
     return web.app.test_client()
+
+
+@pytest.fixture
+def static_app(tmpdir):
+    web_static.app.config['SITE_DIR'] = str(tmpdir)
+    return web_static.app.test_client()
 
 
 @pytest.yield_fixture
---
+++ tests/test_web_static.py
@@ -0,0 +1,28 @@
+import json
+
+
+def test_index_redirects_to_api(static_app):
+    response = static_app.get('/')
+
+    assert response.status_code == 302
+    assert response.headers['Location'].endswith('/api/')
+
+
+def test_path_that_does_not_exist(static_app):
+    response = static_app.get('/foo/')
+
+    assert response.status_code == 404
+
+
+def test_path_returns_correct_file_as_json(static_app, tmpdir):
+    index_file = tmpdir.mkdir('foo').join('index.html')
+    with index_file.open('w') as fh:
+        json.dump({'foo': 'bar'}, fh)
+
+    response = static_app.get('/foo/')
+
+    assert response.status_code == 200
+    assert response.mimetype == 'application/json'
+
+    data = json.loads(response.data.decode('utf-8'))
+    assert data == {'foo': 'bar'}
